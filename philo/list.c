618a0379182f348d1e5ab27173723a96cfc3a07e
fcaquard <fcaquard@student.s19.be>
2022-02-12 17:03:31 +0100

    norminette list.c

--- philo/list.c
+++ philo/list.c
@@ -6,13 +6,14 @@
 /*   By: fcaquard <fcaquard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/31 16:57:39 by fcaquard          #+#    #+#             */
-/*   Updated: 2022/02/08 18:31:41 by fcaquard         ###   ########.fr       */
+/*   Updated: 2022/02/12 17:03:06 by fcaquard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-t_list	*list_rewind(t_list *list)
+// only used up until the list is a loop 
+static t_list	*list_rewind(t_list *list)
 {
 	while (list)
 	{
@@ -23,7 +24,8 @@ t_list	*list_rewind(t_list *list)
 	return (list);
 }
 
-void	clear_list(t_list **list)
+// uses list_rewind, so only if the list isn't a loop
+static void	clear_list(t_list **list)
 {
 	t_list	*tmp;
 
@@ -43,6 +45,27 @@ void	clear_list(t_list **list)
 		free(*list);
 		*list = tmp;
 	}
+}
+
+// clears the whole list on error
+static t_list	*init_list(t_list **previous)
+{
+	t_list	*list;
+
+	list = malloc(sizeof(t_list) * 1);
+	if (!list)
+	{
+		clear_list(previous);
+		return (NULL);
+	}
+	list->prev = NULL;
+	list->next = NULL;
+	if (*previous)
+	{
+		list->prev = *previous;
+		(*previous)->next = list;
+	}
+	return (list);
 }
 
 void	clear_loop_list(size_t size, t_list **list)
@@ -68,26 +91,7 @@ void	clear_loop_list(size_t size, t_list **list)
 	}
 }
 
-t_list	*init_list(t_list **previous)
-{
-	t_list	*list;
-
-	list = malloc(sizeof(t_list) * 1);
-	if (!list)
-	{
-		clear_list(previous);
-		return (NULL);
-	}
-	list->prev = NULL;
-	list->next = NULL;
-	if (*previous)
-	{
-		list->prev = *previous;
-		(*previous)->next = list;
-	}
-	return (list);
-}
-
+// returns NULL on error (list already cleared)
 t_list	*generate_list(size_t i)
 {
 	t_list	*list;
@@ -99,7 +103,6 @@ t_list	*generate_list(size_t i)
 		list = init_list(&list);
 		if (!list)
 			return (NULL);
-		
 	}
 	first = list_rewind(list);
 	list->next = first;
