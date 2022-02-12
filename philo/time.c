699efca93489373eef66b03cf417d8349ccd7dc8
fcaquard <fcaquard@student.s19.be>
2022-02-12 17:13:55 +0100

    norminette time.c

--- philo/time.c
+++ philo/time.c
@@ -6,19 +6,20 @@
 /*   By: fcaquard <fcaquard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 15:42:55 by fcaquard          #+#    #+#             */
-/*   Updated: 2022/02/12 11:49:57 by fcaquard         ###   ########.fr       */
+/*   Updated: 2022/02/12 17:12:11 by fcaquard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./philo.h"
 
-size_t	timestamp_ms()
+size_t	timestamp_ms(void)
 {
 	t_timeval		tv;
 	size_t			time;
 	static size_t	simulation_start = 0;
 
-	while (gettimeofday(&tv, NULL) == -1) ;
+	while (gettimeofday(&tv, NULL) == -1)
+		;
 	time = ((tv.tv_sec * 1000000) + tv.tv_usec) / 1000;
 	if (simulation_start == 0)
 		simulation_start = time;
